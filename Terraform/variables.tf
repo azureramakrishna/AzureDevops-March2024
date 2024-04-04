12f537d7a2c22766dfdea07477b1a79e606b906d
ramakrishna <saanvikitfeb2024@gmail.com>
2024-04-04 08:58:14 +0530

    terraform azure vm

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -8,6 +8,10 @@ variable "location" {
   default = "uksouth"
 }
 
+variable "client_secret" {
+  type = string
+}
+
 variable "storage_account_name" {
   type    = string
   default = "TerraformSa20240403"
@@ -18,4 +22,59 @@ variable "tags" {
     project     = "saanvikit"
     environment = "DEV"
   }
+}
+
+variable "virtual_network_name" {
+  type    = string
+  default = "terraform-vnet"
+}
+
+variable "virtual_network_address" {
+  type    = list(string)
+  default = ["172.16.0.0/24"]
+}
+
+variable "subnet_name" {
+  type    = string
+  default = "terraform-snet"
+}
+
+variable "subnet_address" {
+  type    = list(string)
+  default = ["172.16.0.0/24"]
+}
+
+variable "network_security_group_name" {
+  type    = string
+  default = "terraform-nsg"
+}
+
+variable "network_interface_name" {
+  type    = string
+  default = "terraform-nic"
+}
+
+variable "public_ip_name" {
+  type    = string
+  default = "terraform-pip"
+}
+
+variable "virtual_machine_name" {
+  type    = string
+  default = "terraform-vm"
+}
+
+variable "virtual_machine_size" {
+  type    = string
+  default = "Standard_Ds1_v2"
+}
+
+variable "AdminUser" {
+  type    = string
+  default = "azureuser"
+}
+
+variable "AdminPassword" {
+  type    = string
+  default = "Azuredevops@12345"
 }
