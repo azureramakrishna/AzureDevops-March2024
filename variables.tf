9779984acc90f3c543038c65709423824e714106
azureramakrishna <83686453+azureramakrishna@users.noreply.github.com>
2024-04-12 08:28:57 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "rsource_group_name" {
+  type    = string
+}
+
+variable "location" {
+  type    = string
+}
+
+variable "storage_account_name" {
+  type    = string
+}
+
+variable "tags" {
+}
