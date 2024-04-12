59ecd1ee2544168ad44dbe484d490e961f6fb270
azureramakrishna <83686453+azureramakrishna@users.noreply.github.com>
2024-04-12 08:27:35 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+# Create a storage account
+resource "azurerm_storage_account" "sa" {
+  name                     = lower(var.storage_account_name)
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = var.tags
+}
