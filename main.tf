b0c83ccf54f75ff37b301a3a9898d890b1029135
azureramakrishna <83686453+azureramakrishna@users.noreply.github.com>
2024-04-12 08:38:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+# Create a resource group
+resource "azurerm_resource_group" "rg" {
+  name     = var.rsource_group_name
+  location = var.location
+}
+
 # Create a storage account
 resource "azurerm_storage_account" "sa" {
   name                     = lower(var.storage_account_name)
